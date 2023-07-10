bd5d73b1c8d2b35f9cd0f5b76ffeeaaa22fa4022
Cekretik <erofeev.fil9@bk.ru>
2023-07-10 19:50:07 +0600

    Update app.go

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ import (
 var db *gorm.DB
 
 func Connect() {
-	dsn := "host=localhost user=postgres password=gopher dbname=bookapp port=1337 sslmode=disable TimeZone=America/New_York"
+	dsn := "host=localhost user=postgres password=gopher dbname=bookapp port=5432 sslmode=disable TimeZone=America/New_York"
 	var err error
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
