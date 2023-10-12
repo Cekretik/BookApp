8523b1dedb4cae22bf50188fd2ce95aa4938968d
Cekretik <erofeev.fil9@bk.ru>
2023-10-12 20:54:55 +0600

    add auth handlers

--- pkg/auth/auth_handler.go
+++ pkg/auth/auth_handler.go
@@ -18,4 +18,34 @@ func Login(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
+	user, err := getUserByUsername(creds.Username)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if !user.checkPasswordHash(creds.Password, user.Password) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid password"})
+		return
+	}
+}
+
+func Register(c *gin.Context) {
+	var creds Credentials
+
+	if err := c.ShouldBindJSON(&creds); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "You have successfully registered"})
+
+}
+
+func getUserByUsername(username string) (*User, error) {
+	var user User
+	if err := db.Where("username = ?", username).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
